c677168572ce426ade56fd1e7cb31ae5ac7a1449
Athapaththu <it21326240@my.sliit.lk>
2024-01-05 22:41:36 +0530

    change port

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
